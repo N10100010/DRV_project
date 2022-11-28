8e7b2574fd7687c99cf5dffc485bebf99158578d
harrif020 <hf020@hdm-stuttgart.de>
2022-11-28 20:57:54 +0100

    added current state for pdf extraction

---
+++ backend/scraping_wr/pdf_result.py
@@ -0,0 +1,246 @@
+"""
+####################################################################################################
+
+This module extracts the data from results pdf files.
+
+Example file:
+https://d3fpn4c9813ycf.cloudfront.net/pdfDocuments/JWCH_2014/JWCH_2014_ROWWSCULL1--J---------SFNL000100--_C73X4443.pdf
+
+Basic stats:
+* Extraction time per file: approx. 1.2 - 1.6s
+####################################################################################################
+"""
+
+import re
+import logging
+import camelot
+from tqdm import tqdm
+import pandas as pd
+
+from api import get_competition_ids, get_pdf_urls
+from utils_general import write_to_json
+from utils_pdf import (clean, clean_df, get_string_loc,
+                       handle_table_partitions, clean_str, print_stats)
+
+BASE_URL = "https://world-rowing-api.soticcloud.net/stats/api/competition/"
+FILTER_STRING = "/?include=pdfUrls.orisCode,events.pdfUrls,events.races.pdfUrls.orisCode,events.boatClass,events.races.racePhase,events.races.photoFinishImage,events.races.raceBoats.raceBoatIntermediates.distance"
+SORT_STRING = "&sortInclude[pdfUrls.created_at]=asc&sortInclude[events.pdfUrls.DisplayName]=desc&sortInclude[events.races.pdfUrls.created_at]=asc"
+
+NO_OF_COMPETITIONS = 300
+INTERMED_INTERVAL = 500
+DISTS = ['500m', '1000m', '1500m', '2000m']
+
+
+def get_athletes(df: pd.DataFrame, rows: int, i: int, n_loc: list) -> list:
+    """
+    Handles extraction of athlete names.
+    -----------------------
+    Parameters:
+    * df:       DataFrame
+    * c_row:    Row index of country, a.k.a. start point of names for given country
+    * i:        Index of current country with regard to countries in given pdf
+    * n_loc:    List with column indices for columns containing names
+    -----------------------
+    Returns:    List with athlete names
+    """
+    # slice part of dataframe containing athlete names for given country
+    # rows: starts at row of current country, ends at row of following country
+    # cols: starts at first occurrence of name col and ends at last occurrence of name col
+    start_row = rows[i]
+    end_row = rows[i+1] if (i + 1 < len(rows)) else df.shape[0]
+    start_col = n_loc[0]
+    # extra margin because some names are one column right of where the names string was found
+    end_col = n_loc[len(n_loc)-1]+2
+    athlete_df = df.iloc[start_row:end_row, start_col:end_col]
+    # remove none values and convert to 1-d list
+    names = athlete_df.dropna().values.reshape(-1)
+    # clean list for relevant data
+    names = clean_str(names, style='name')
+    names = list(filter(None, names))
+
+    return names
+
+
+def get_times(df: pd.DataFrame, row: int, cols: list) -> dict:
+    """
+    Handles extraction of intermediate times.
+    ----------------------
+    Parameters:
+    * df:   DataFrame
+    * row:  Row index of Country
+    * cols: List of columns containing intermediate times, e.g. 500m, 1000m
+    """
+    vals, times, dns = [], {}, False
+    for col in cols:
+        time = df.iloc[row:row+1, col].values[0]
+        # TODO: Create list with alternatives...
+        if "DNS" in str(time) or "DNF" in str(time) or "BUW" in str(time):
+            logging.warning(' DNS time found.')
+            dns = True
+        vals.extend(re.findall(r"\d{1,2}\:\d{2}\.\d{2}", str(time)))
+    time_strings = clean_str(vals, style='time')
+
+    # Check how many time values have to be extracted
+    data_list = df.values.tolist()
+    data = ''.join(str(el) for el in data_list)
+    num_of_times = [x for x in DISTS if x in data]
+
+    if not dns:
+        assert len(time_strings) == len(num_of_times), "Not enough times found"
+    for key, time in enumerate(time_strings):
+        dict_key = (key+1) * INTERMED_INTERVAL
+        times[dict_key] = time
+
+    return times
+
+
+def get_intermediate_ranks(df: pd.DataFrame, row: int) -> list:
+    times_row = df.iloc[row:row+1, :].values.tolist()
+    ranks = re.findall(r"\((\d{1})\)", str(times_row))
+    return [int(rank) for rank in ranks if rank]
+
+
+def get_country_code(df: pd.DataFrame, row: int, cols: list) -> str:
+    """
+    Handles extraction of country code.
+    ----------------
+    Parameters:
+    * df:   DataFrame
+    * row:  Row index of country
+    * cols: List of column indices containing country codes
+    ---------------
+    Returns: string representing the country code
+    """
+    start_row = row
+    end_row = row+1
+    start_col = cols[0]
+    end_col = cols[-1]+1
+
+    country_df = df.iloc[start_row:end_row, start_col:end_col]
+    country_data = country_df.values.reshape(-1)
+    country = clean_str(country_data, style="country")
+    return list(filter(None, country))[0]
+
+
+def get_lane(df: pd.DataFrame, row: int, i: int, cols: int) -> tuple[int: int]:
+    start_row = row
+    end_row = row+1
+    start_col = cols[0]
+    end_col = cols[-1]+1
+
+    lane_df = df.iloc[start_row:end_row, start_col:end_col]
+    lane_data = lane_df.values.reshape(-1)
+    # Idea here: Lane is either the number that is not the rank (i+1)
+    # If this is not the case then it is equal to the rank
+    lane_string = ''.join(str(el) for el in lane_data)[:2]
+    assert len(lane_string) == 2, "String has not length of 2!"
+    nums = [int(x) for x in lane_string if x.isdigit()]
+    if (len(nums) == 1):
+        return nums[0]
+    elif (len(nums) == 2):
+        num1, num2 = nums
+        if num1 == i+1:
+            return num2
+        elif num2 == i+1:
+            return num1
+        else:
+            return i+1
+
+
+def check_extracted_data(data: dict) -> dict:
+    # restrict number of athletes to most frequent number of athletes
+    lens = [len(v["athletes"]) for v in data.values() if "athletes" in v]
+    max_len = max(set(lens), key=lens.count)
+    for el in data.values():
+        if "athletes" in el:
+            el["athletes"] = el["athletes"][:max_len]
+    return data
+
+
+def extract_result_data(urls: list) -> tuple[list, list]:
+    """
+    This function extracts relevant data from the result data pdfs.
+    --------------
+    Parameters:
+    * urls: list of urls to pdfs
+    --------------
+    Returns: list with extracted data
+    """
+    logging.info(f"Extracting data from {pdf_urls} pdfs.")
+    result_data, failed_requests, errors, empty_files = [], [], 0, 0
+
+    for url in tqdm(urls):
+        try:
+            tables = camelot.read_pdf(
+                url, flavor="stream", pages="all", column_tol=5)
+
+            # prepare df
+            df = clean(handle_table_partitions(tables=tables, results=1))
+            rank_row = get_string_loc(df, rank=True, column=0)["rank"]["row"]
+            # remove everything above the rank row
+            df = df.iloc[rank_row:].copy()
+            df, _ = clean_df(df)
+
+            # get distance and country locations
+            dist_locs = get_string_loc(df, *DISTS)["str"]["col"]
+
+            # get country locations
+            cntry_locs = get_string_loc(df, country=True, results=1)["cntry"]
+            country_rows, country_cols = cntry_locs["row"], cntry_locs["col"]
+
+            # get names column index
+            n_loc = get_string_loc(df, "Name")["str"]["col"]
+
+            assert len(country_rows) > 0
+
+            extraction_result = {}
+            for idx, row in enumerate(country_rows):
+                # extract lane, country, athletes and times via dedicated functions
+                lane = get_lane(df, row=row, i=idx, cols=country_cols)
+                country_code = get_country_code(
+                    df, row=row, cols=country_cols)
+                athletes = get_athletes(
+                    df=df, rows=country_rows, i=idx, n_loc=n_loc)
+                times = get_times(df, row=row, cols=dist_locs)
+                inter_ranks = get_intermediate_ranks(df, row=row)
+
+                extraction_result[idx] = {
+                    "country": country_code,
+                    "rank": idx+1,
+                    "lane": lane,
+                    "athletes": athletes,
+                    "times": times,
+                    "inter_ranks": inter_ranks
+                }
+
+            if not extraction_result:
+                empty_files += 1
+            else:
+                extraction_result["url"] = url
+                # do plausibility checks on extracted data here
+                res = check_extracted_data(extraction_result)
+                result_data.append(res)
+                logging.info(f"Extract of {url.split('/').pop()} successful.")
+
+        except Exception as e:
+            errors += 1
+            failed_requests.append(url)
+            logging.error(f"Error at {url}: {e}.")
+
+    total = len(urls) - empty_files
+    rate = "{:.2f}".format(100 - ((errors / total if total else 0) * 100))
+    print_stats(total=total, errors=errors, empties=empty_files, rate=rate)
+
+    return result_data, failed_requests
+
+
+competition_ids = get_competition_ids(years=2018)
+pdf_urls = get_pdf_urls(comp_ids=competition_ids,
+                        comp_limit=NO_OF_COMPETITIONS, results=1)[:10]  # [::3]
+
+
+pdf_data, failed_req = extract_result_data(urls=pdf_urls)
+
+# write results to file
+write_to_json(data=pdf_data, filename="result_data")
+write_to_json(data=failed_req, filename="result_data_failed")
--- backend/scraping_wr/utils_pdf.py
+++ backend/scraping_wr/utils_pdf.py
@@ -8,14 +8,9 @@ Contains utility functions for pandas and the extraction of world rowing data fr
 ####################################################################################################
 """
 
-
-import traceback
 import re
-import json
 import pandas as pd
 import numpy as np
-import requests
-import jsbeautifier
 
 import logging
 logger = logging.getLogger(__name__)
@@ -252,7 +247,8 @@ def clean(df: pd.DataFrame) -> pd.DataFrame:
 
 def print_stats(total: int, errors: int, empties: int, rate: str) -> None:
     """ Prints basic statistics for the pdf reading process. """
-    logger.info("{txt:-^25}".format(txt=f"\nRead: {(total)-errors}/{total} PDFs | ({rate}%)"))
+    logger.info(
+        "{txt:-^25}".format(txt=f"\nRead: {(total)-errors}/{total} PDFs | ({rate}%)"))
     logger.info("{txt:-^25}".format(txt=f" Empty Files: {empties} "))
 
 
@@ -268,51 +264,6 @@ def clean_convert_to_list(df: pd.DataFrame) -> list:
 WorldRowing Utils
 ################################################################
 '''
-
-
-def get_competition_ids(base_url: str, year: int) -> list:
-    """ Fetches all competition Ids from WorldRowing.com
-    ---------
-    Parameters:
-    * base_url:    https://world-rowing-api.soticcloud.net/stats/api/competition/
-    * year:        specifies year --> in order to not hit the API limit of 1000 ids
-    """
-    year_filter_query = f"?filter[Year]={year}" if year else ''
-    competitions = requests.get(base_url + year_filter_query).json()["data"]
-    ids_list = [comp["id"] for comp in competitions]
-    return ids_list
-
-
-def get_pdf_urls(base_url: str, comp_ids: list, comp_limit: int, filter_str: str, results: bool) -> list:
-    """
-    OBSOLETE
-
-    Fetches URLs to pdf files on https://d3fpn4c9813ycf.cloudfront.net/.
-    ---------
-    Parameters:
-    * base_url:     for world rowing this is https://world-rowing-api.soticcloud.net/stats/api/competition/
-    * comp_ids:     list of competition ids
-    * comp_limit:   limit amount of competitions
-    * filter_str:   filter parameter and values as string, e.g. '?include=events.races<...>'
-    * results:      0 = "Race Data" | 1 = "Results"
-    ---------
-    Returns: list of urls for specified criteria
-    """
-    competition_ids = comp_ids[0:comp_limit]
-    urls = []
-    doc_type = "Results" if results else "Race Data"
-
-    for comp_id in competition_ids:
-        query = base_url + comp_id + filter_str
-        events = requests.get(query).json()["data"]["events"]
-
-        for event in events:
-            races = event["races"]
-            for race in races:
-                for pdf in race["pdfUrls"]:
-                    if pdf["title"] == doc_type:
-                        urls.append(pdf["url"])
-    return urls
 
 
 def get_string_loc(df: pd.DataFrame, *args: str, country: bool = False, rank: bool = False, first: bool = False, column: int = -1, results: bool = 0) -> dict:
@@ -362,23 +313,32 @@ def get_string_loc(df: pd.DataFrame, *args: str, country: bool = False, rank: bo
         else:
             row_lst = []
             for col in df.columns:
-                occ_idx = df.loc[df[col].isin(codes)].index
-                row = int(occ_idx[0]) if occ_idx.size > 0 else None
+                occurence = df.loc[df[col].str.contains('|'.join(codes))]
+                row = int(occurence.index[0]) if occurence.size > 0 else None
                 if isinstance(row, int):
                     row_lst.append(row)
             if row_lst:
                 locs["cntry"]["row"] = [max(set(row_lst), key=row_lst.count)]
 
     elif country and results:
+
         # For for results pdfs we want to find all row-wise occurrences of country codes
-        counter, row_idxs, country_codes = 0, [], '|'.join(codes)
+        # even if they are in multiple columns
+        i, row_idxs, country_codes = 0, [], '|'.join(codes)
         # from col 0 upwards check where country codes can be found
-        while len(row_idxs) == 0 and counter < df.shape[1]-1:
-            find_row_idxs = df[counter].str.contains(country_codes) == True
+        while len(row_idxs) == 0 and i < df.shape[1]-1:
+            find_row_idxs = df[i].str.contains(country_codes) == True
             row_idxs = df.index[find_row_idxs].tolist()
-            locs["cntry"]["col"] = counter
-            counter += 1
-        locs["cntry"]["row"] = row_idxs
+            # only check the following column if there are no athlete names
+            next_col = df[i+1].isin([country_codes]).any()
+            if next_col:
+                locs["cntry"]["col"].extend([i, i+1])
+                next_row_idxs = df.index[next_col].tolist()
+                locs["cntry"]["row"].extend(next_row_idxs)
+            else:
+                locs["cntry"]["col"].append(i)
+            i += 1
+        locs["cntry"]["row"].extend(row_idxs)
 
     if rank and not column == -1 and not df.empty:
         ranks_contained = df[column].str.contains(rank_str, na=False)
@@ -387,6 +347,11 @@ def get_string_loc(df: pd.DataFrame, *args: str, country: bool = False, rank: bo
             rank_row = df.index[ranks_contained][0]
             locs["rank"]["row"] = rank_row
             locs["rank"]["col"] = 0
+
+    country_row_data = locs["cntry"]["row"]
+    country_row_data = list(set(country_row_data))
+    country_row_data.sort()
+    locs["cntry"]["row"] = country_row_data
 
     return locs
 
@@ -401,22 +366,25 @@ def get_data_loc(df: pd.DataFrame, cust_str: str = '') -> tuple[int, int]:
     Returns: tuple of row indices (start, end) of race data.
     """
     start, end = 0, 0
-    interval_10, interval_25, interval_50 = DIST_INTERVALS
+    first_column = df[0].values.flatten().tolist()
+    dists = [re.sub(r"\n.*", "", str(x)) for x in first_column]
 
     try:
         if cust_str and cust_str in df.values:
             start = get_string_loc(df, cust_str, column=0)["str"]["row"]
-            end = last_num_idx(df)
+        elif cust_str in dists:
+            start = dists.index(cust_str)
         else:
-            if interval_10 in df.values:
-                start = get_string_loc(df, interval_10, column=0)["str"]["row"]
-                end = last_num_idx(df)
-            elif interval_25 in df.values:
-                start = get_string_loc(df, interval_25, column=0)["str"]["row"]
-                end = last_num_idx(df)
-            elif interval_50 in df.values:
-                start = get_string_loc(df, interval_50, column=0)["str"]["row"]
-                end = last_num_idx(df)
+            for i in DIST_INTERVALS:
+                if i in df.values:
+                    start = get_string_loc(df, i, column=0)["str"]["row"]
+            if start == 0 and end == 0:
+                for i in DIST_INTERVALS:
+                    try:
+                        start = dists.index(i)
+                    except ValueError as e:
+                        logging.warn(f"No start index found: {e}")
+        end = last_num_idx(df)
 
     except Exception as e:
         logging.error(f"Error finding data start/end: \n{e}")
@@ -452,8 +420,9 @@ def handle_table_partitions(tables, results: bool = 0) -> pd.DataFrame:
             if not first_df.empty:
                 if results == 0:  # only apply on race data pdfs
                     first_loc = get_data_loc(first_df)
-                    # distance interval, e.g. 25, 50 depending on scale
-                    interval = get_dist_interval(first_df, row_range=first_loc)
+                    # get distance interval, e.g. 25, 50 depending on scale
+                    first_tab = first_df.apply(pd.to_numeric, errors='coerce')
+                    interval = first_tab[0].diff().mode()[0].astype("int")
                     df = df.append(first_df, ignore_index=True)
                     # set table_end to current last value of df
                     tab_end = int(first_df.iat[first_loc[1], 0])
@@ -465,56 +434,57 @@ def handle_table_partitions(tables, results: bool = 0) -> pd.DataFrame:
         elif idx > 0 if results else (idx > 0 and tab_end != int(RACE_DIST)):
             next_df = table
             if not next_df.empty:
-                next_start = tab_end + interval
-                next_loc = get_data_loc(df=next_df, cust_str=str(next_start))
-                # set table_end to current last value of df
-                tab_end = last_num_idx(next_df)
-                next_df = next_df.iloc[next_loc[0]:next_loc[1]+1]
-                # append next_df part to main df
-                df = df.append(next_df, ignore_index=True)
-
+                if results == 0:
+                    next_start = tab_end + interval
+                    next_loc = get_data_loc(
+                        df=next_df, cust_str=str(next_start))
+                    # set table_end to current last value of df
+                    tab_end = last_num_idx(next_df)
+                    next_df = next_df.iloc[next_loc[0]:next_loc[1]+1]
+                    # append next_df part to main df
+                    df = df.append(next_df, ignore_index=True)
+                else:
+                    rank_row = get_string_loc(next_df, rank=True, column=0)[
+                        "rank"]["row"]
+                    # remove everything above the rank row
+                    next_df = next_df.iloc[rank_row:].copy()
+                    df = df.append(next_df, ignore_index=True)
     return df
 
 
-def get_dist_interval(df, row_range: tuple, col: int = 0) -> int:
-    """ Finds and returns most frequent Interval in specified column of df.
-    ----------------
-    Parameters:
-    * row_range:    contains start/end of relevant rows
-    * col:          column index to check for interval
-    ----------------
-    Returns: Number for interval, e.g. 50, 25 etc.
-    """
-    assert any(row_range), "Distance interval could not be identified."
-    dist_cols = df[col].iloc[row_range[0]:row_range[1]+1]
-    diffs = dist_cols.astype('int').diff().to_numpy()
-    values, counts = np.unique(diffs, return_counts=True)
-    return int(values[np.argmax(counts)])
-
-
-def apply_regex(regexs: list, repl: str = '', input_str: str = ''):
-    new_str = input_str
+def apply_regex_sub(regexs: list, repl: str = '', input: str = ''):
+    """ applies regex sub on input string and returns resulting string. """
+    new_str = input
     for regex in regexs:
         new_str = re.sub(regex, repl, new_str)
     return new_str
 
 
-def clean_str(string: str, str_type: str):
+def clean_str(str_list: list, style: str):
     """ Handles regular expressions to filter for specific data formats.
     -----------
     Parameters:
-    * string:       input string
-    * str_type:     time/name/country/number depending on the desired format
+    * string:   input string
+    * style:    time/name/country/number depending on the desired format
     -----------
     Returns:    Filtered string
     """
     type_dict = {
         "time": [r"\(.*?\)", r"[^0-9.:]"],
-        "name": [r"[0-9.:]"],
+        "name": [r"[0-9.:]", r"\(.*?\)"],
         "country": [r"\(.*?\)", r"[0-9]"],
-        "number": [r"\d+$", r"\D"]
+        "number": [r"\d+$", r"\D"],
+        "dist": [r"\d*\.\d+", r"\n", r"[^0-9,]", r"\n.*"]
     }
-    return apply_regex(regexs=type_dict.get(str_type), repl='', input_str=string)
+    final = []
+    for el in str_list:
+        final.append(apply_regex_sub(
+            regexs=type_dict.get(style), repl='', input=str(el)))
+    filter_list = [None, "0", ""]
+    final_list = list(filter(lambda el: el not in filter_list, final))
+    if style == "dist":
+        final_list = list(map(int, final_list))
+    return final_list
 
 
 def handle_edge_cases(df: pd.DataFrame, results=0) -> pd.DataFrame:
@@ -528,74 +498,9 @@ def handle_edge_cases(df: pd.DataFrame, results=0) -> pd.DataFrame:
 
     # Edge Case 1: Dataframe contains no country and no rank (often the table head)
     # if no country found and no rank found discard table by returning empty dataframe
-
     if cnty_idx and (rank_idx if results else True):
         return df
     return pd.DataFrame()
-
-
-def handle_dist_edge_case(df: pd.DataFrame) -> pd.DataFrame:
-    """ Handles explicit edge cases for result PDFs when race distance
-    columns are not identified as separate columns.
-    ------------------
-    Returns: fixed dataframe
-    """
-    comb_lists = [
-        # --> split to 2 cols
-        ["500m1000m", "1500m2000m", "Name500m1000m", "1000m1500m"],
-        # --> split to 3 cols
-        ["500m1000m1500m", "Name500m1000m1500m"],
-        # --> split to 4 cols
-        ["500m1000m1500m2000m"]
-    ]
-    for idx, col_list in enumerate(comb_lists):
-        values = df.iloc[0].values
-        loc, found_str = 0, ''
-
-        for i, el in enumerate(values):
-            if el in col_list:
-                loc, found_str = i, el
-
-        if loc and idx == 0:
-            comb_col = df[loc].squeeze()
-            # add one extra column
-            df.insert(loc+1, loc+.1, 0)
-            names = [float(loc), loc+.1]
-            df[names] = comb_col.str.split(r'\(\d?\)', n=1, expand=True)
-            df.set_axis(
-                list(np.arange(0, df.shape[1])), axis=1, inplace=True)
-            # set new col names
-            if found_str == comb_lists[idx][0]:
-                df.at[0, loc] = "Name500m"
-                df.at[0, loc+1] = "Name1000m"
-            elif found_str == comb_lists[idx][1]:
-                df.at[0, loc] = "1500m"
-                df.at[0, loc+1] = "2000m"
-            elif found_str == comb_lists[idx][2]:
-                df.at[0, loc] = "Name500m"
-                df.at[0, loc+1] = "Name1000m"
-            elif found_str == comb_lists[idx][3]:
-                df.at[0, loc] = "1000m"
-                df.at[0, loc+1] = "1500m"
-
-        elif loc and idx == 1:
-            comb_col = df[loc].squeeze()
-            # add two extra cols
-            df.insert(loc+1, loc+.1, 0)
-            df.insert(loc+2, loc+.2, 0)
-            names = [float(loc), loc+.1, loc+.2]
-            df[names] = comb_col.str.split(r'\(\d?\)', n=2, expand=True)
-            df.set_axis(
-                list(np.arange(0, df.shape[1])), axis=1, inplace=True)
-            # set new col names
-            df.at[0, loc] = "Name500m"
-            df.at[0, loc+1] = "Name1000m"
-            df.at[0, loc+2] = "Name1500m"
-
-        elif loc and idx == 2:
-            # Placeholder for edge case when there are four columns to split.
-            logging.warning("Not implemented yet. Data needs to be split to four columns.")
-    return df
 
 
 '''
@@ -609,7 +514,7 @@ def last_num_idx(df: pd.DataFrame, col: int = 0) -> int:
     """ Finds and returns index of last numeric value in DataFrame."""
     values = list(reversed(df[col].values))
     for idx, el in enumerate(values):
-        if el.isdigit():
+        if el.isdigit() and len(el) > 2:
             return (len(values)-1)-idx
     return 0
 
