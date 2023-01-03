9f1130c6d4041bb9c345eca7d801ac9347b1e3b4
raphaelw <raphaelw@users.noreply.github.com>
2023-01-03 14:22:02 +0100

    Update model.py

--- backend/model/model.py
+++ backend/model/model.py
@@ -34,6 +34,12 @@ TODO:
     - https://worldrowing.com/event/2021-world-rowing-final-paralympic-qualification-regatta -> same
     - Wettkampfklassen / Competition Classes
 
+    - !!! CountryCodes are not official
+    - ??? communityTypes -> U23, Senior, ...
+        -> https://worldrowing.com/event/2022-world-rowing-under-23-championships
+        -> https://world-rowing-api.soticcloud.net/stats/api/competition/7ee519e5-288d-4585-bab4-bf916dca49b8?include=venue,venue.country,competitionType.competitionCategory,communityTypes
+        -> BEWARE: Multiple communities: https://world-rowing-api.soticcloud.net/stats/api/competition/718b3256-e778-4003-88e9-832c4aad0cc2?include=venue,venue.country,competitionType.competitionCategory,communityTypes
+
     - world-rowing GUIDs -> As String or Postgres UUID ?
     - race data (high res)
     - Specify Not Null Columns
@@ -46,10 +52,27 @@ TODO:
     - No data source hints are written into db (?)
     - INCLUDE PDFs!!! -> More maximalist apporach
 
+    - Timestamp and timezones:
+        https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-timestamp/
+        https://codeofmatt.com/please-dont-call-it-epoch-time/#:~:text=Unix%20timestamps%20are%20always%20based,without%20any%20leaps%20ever%20occurring.
+
 
 https://worldrowing.com/event/2022-world-rowing-cup-iii
 https://world-rowing-api.soticcloud.net/stats/api/race/?include=racePhase%2Cevent.competition.competitionType%2Cevent.competition.competitionType.competitionCategory%2Cevent.boatClass&filter%5Bevent.competitionId%5D=b56cf9a5-a7d3-4e64-9571-38218f39413b&sort%5Bdate%5D=asc
 """
+
+class Countries(Base):
+    __tablename__ = "countries"
+
+    id = Column(Integer, primary_key=True)
+    additional_id_ = Column(String)
+
+    country_code = Column(String(length=3))
+    name = Column(String)
+
+    # TODO: Clarify meaning and types
+    is_former_country_ = Column(String)
+    is_noc_ = Column(String)
 
 
 class Venues(Base):
@@ -60,12 +83,22 @@ class Venues(Base):
 
     id = Column(Integer, primary_key=True)
     additional_id_ = Column(String)
+    country_id = Column(Integer, ForeignKey("countries.id"))
 
-    country_code = Column(String(length=3))
     city = Column(String)
     site = Column(String)
 
     is_world_rowing_venue = Column(Boolean)
+
+
+class Genders(Base):
+    __tablename__ = "genders"
+
+    id = Column(Integer, primary_key=True)
+    additional_id_ = Column(String)
+
+    name = Column(String)
+
 
 class Athletes(Base):
     __tablename__ = "athletes"
@@ -75,9 +108,7 @@ class Athletes(Base):
 
     full_name = Column(String)
     birthdate = Column(Date)
-    gender_id = Column(Integer) # TODO: gender table
-
-    # TODO: country?
+    # TODO: gender? already contained in Event entity
 
 
 class Boat_Classes(Base):
@@ -85,11 +116,9 @@ class Boat_Classes(Base):
 
     id = Column(Integer, primary_key=True)
     additional_id_ = Column(String)
-    
-    abbreviation = Column(String, nullable=False)
-    display_name = Column(String, nullable=False)
 
-    gender_id = Column(Integer) # TODO: gender table
+    abbreviation = Column(String, nullable=False)
+    name = Column(String)
 
 
 class Competition_Category(Base):
@@ -109,7 +138,7 @@ class Competitions(Base):
     id = Column(Integer, primary_key=True)
     additional_id_ = Column(String)
 
-    competition_class_id = Column(Integer, ForeignKey("competition_category.id"))
+    competition_category_id = Column(Integer, ForeignKey("competition_category.id"))
     venue_id = Column(Integer, ForeignKey("venues.id"))
 
     name = Column(String)
@@ -125,31 +154,62 @@ class Events(Base):
 
     id = Column(Integer, primary_key=True)
     additional_id_ = Column(String)
-
+    name = Column(String)
 
     competition_id = Column(Integer, ForeignKey("competitions.id"))
     boat_class_id = Column(Integer, ForeignKey("boat_classes.id"))
+    gender_id = Column(Integer, ForeignKey("genders.id"))
 
-    name = Column(String)
+    rsc_code_ = Column(String)
+
 
 class Races(Base): # https://world-rowing-api.soticcloud.net/stats/api/race/b0eae369-8d05-4b8e-9a2e-7de5871715b7?include=racePhase%2CraceBoats.raceBoatAthletes.person%2CraceBoats.invalidMarkResultCode%2CraceBoats.raceBoatIntermediates.distance&sortInclude%5BraceBoats.raceBoatIntermediates.ResultTime%5D=asc
     __tablename__ = "races"
 
     id = Column(Integer, primary_key=True)
+    additional_id_ = Column(String)
     event_id = Column(Integer, ForeignKey("events.id"))
+    name = Column(String)
 
-    name = Column(String) # e.g. "FA", "H3", "SA/B1", etc...
-    phase = Column(String) # e.g. "Heat", "Final" // TODO: normalize?
+    date = Column(Date)
+    phase_type = Column(String) # e.g. "Heat", "Final" // TODO: normalize?
+    phase = Column(String) # e.g. "FA", "H3", "SA/B1", etc...
 
-    # TODO: add RscCode?
+    progression = Column(String) # e.g. "1-2->SA/B, 3..->R"
+    rsc_code_ = Column(String)
+
+    pdf_url_results = Column(String)
+    pdf_url_race_data = Column(String)
+
+    # Meaning and importance not exactly clear
+    race_nr__ = Column(String) # e.g. "103"
+    rescheduled__ = Column(Integer) # e.g. 0 # maybe map to bool?
+    rescheduled_from__ = Column(String) # low-prio TODO: Type unclear
+    race_status = Column(String) # e.g. DisplayName "Official" id "182f6f15-8e78-41c3-95b3-8b006af2c6a1"
 
 
-class Boats(Base):
-    __tablename__ = "boats"
+class RaceBoats(Base):
+    __tablename__ = "raceboats"
 
     id = Column(Integer, primary_key=True)
+    additional_id_ = Column(String)
     race_id = Column(Integer, ForeignKey("races.id"))
-    result_time = Column(Interval) # TODO: Simply milliseconds as Integer?
+    country_id = Column(Integer, ForeignKey("countries.id"))
+
+    name = Column(String) # e.g. "GER2" for one of the German boats
+
+    result_time_ms = Column(Integer) # milliseconds as Integer
+
+    lane = Column(Integer) # e.g. 1
+    rank = Column(Integer) # e.g. 2
+    final_rank = Column(Integer) # e.g. 8
+    final_rank_index__ = Column(String) # TODO: Meaning, Importance, Type?
+
+    # TODO: Meaning, importance and/or type unclear
+    remark__ = Column(String)
+    world_cup_points__ = Column(Integer)
+    club_name__ = Column(String)
+    # worldBestTimeId, OVRCode ?
 
 
 class Race_Data(Base):
