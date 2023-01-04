62665f763bdf662715bf7e531887a8598c4bef0e
raphaelw <raphaelw@users.noreply.github.com>
2023-01-04 17:25:21 +0100

    db-model: add relationship descriptions

--- backend/model/model.py
+++ backend/model/model.py
@@ -18,9 +18,9 @@ logging.getLogger().setLevel(logging.INFO)
 # engine = create_engine("sqlite:///foo.sqlite", echo=True)
 
 # postgres
-engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/rowing", echo=True)
+# engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/rowing", echo=True)
 
-Session = sessionmaker(bind=engine)
+# Session = sessionmaker(bind=engine)
 
 # connect will actually establish a connection or create/load the sqlite file
 # engine.connect()
@@ -43,7 +43,7 @@ TODO:
     - world-rowing GUIDs -> As String or Postgres UUID ?
     - race data (high res)
     - Specify Not Null Columns
-    - add backref / back_populates for easy access
+    - add backref / back_populates for easy access ---> use back_populates for clean code
     - add athlete names
     - add progression string
     - Consider foreign key checking via constraints
@@ -56,6 +56,7 @@ TODO:
         https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-timestamp/
         https://codeofmatt.com/please-dont-call-it-epoch-time/#:~:text=Unix%20timestamps%20are%20always%20based,without%20any%20leaps%20ever%20occurring.
 
+    - Query ORM Examples: https://docs.sqlalchemy.org/en/14/orm/queryguide.html#select-statements
 
 https://worldrowing.com/event/2022-world-rowing-cup-iii
 https://world-rowing-api.soticcloud.net/stats/api/race/?include=racePhase%2Cevent.competition.competitionType%2Cevent.competition.competitionType.competitionCategory%2Cevent.boatClass&filter%5Bevent.competitionId%5D=b56cf9a5-a7d3-4e64-9571-38218f39413b&sort%5Bdate%5D=asc
@@ -101,11 +102,15 @@ class Venue(Base):
     id = Column(Integer, primary_key=True)
     additional_id_ = Column(String)
     country_id = Column(Integer, ForeignKey("countries.id"))
+    country    = relationship("Country")
 
     city = Column(String)
     site = Column(String)
 
     is_world_rowing_venue = Column(Boolean)
+
+    # relationships
+    competitions = relationship("Competition", back_populates="venue")
 
 
 class Gender(Base):
@@ -135,6 +140,9 @@ class Athlete(Base):
     # TODO: gender? already contained in Event entity
     # OVRCode?
 
+    # relationships
+    country = relationship("Country")
+
 class Boat_Class(Base):
     __tablename__ = "boat_classes"
 
@@ -143,6 +151,9 @@ class Boat_Class(Base):
 
     abbreviation = Column(String, nullable=False)
     name = Column(String)
+
+    # relationships
+    events = relationship("Event", back_populates="boat_class")
 
 
 class Competition_Category(Base):
@@ -154,6 +165,9 @@ class Competition_Category(Base):
 
     name = Column(String)
 
+    # relationships
+    competitions = relationship("Competition", back_populates="competition_category")
+
 
 class Competition(Base):
     """https://world-rowing-api.soticcloud.net/stats/api/competition/718b3256-e778-4003-88e9-832c4aad0cc2?include=venue,competitionType"""
@@ -163,13 +177,18 @@ class Competition(Base):
     additional_id_ = Column(String)
 
     competition_category_id = Column(Integer, ForeignKey("competition_category.id"))
+    competition_category    = relationship("Competition_Category", back_populates="competitions")
     venue_id = Column(Integer, ForeignKey("venues.id"))
+    venue    = relationship("Venue", back_populates="competitions")
 
     name = Column(String)
     start_date = Column(Date)
     end_date = Column(Date)
     competition_code_ = Column(String)
     is_fisa_ = Column(Boolean)
+
+    # relationships
+    events = relationship("Event", back_populates="competition")
     
 
 class Event(Base):
@@ -181,10 +200,16 @@ class Event(Base):
     name = Column(String)
 
     competition_id = Column(Integer, ForeignKey("competitions.id"))
+    competition    = relationship("Competition", back_populates="events")
     boat_class_id = Column(Integer, ForeignKey("boat_classes.id"))
+    boat_class    = relationship("Boat_Class", back_populates="events")
     gender_id = Column(Integer, ForeignKey("genders.id"))
+    gender    = relationship("Gender")
 
-    rsc_code_ = Column(String)
+    rsc_code_ = Column(String) # RSC-Codes of races contain more information
+
+    # relationships
+    races = relationship("Race", back_populates="event")
 
 
 class Race(Base): # https://world-rowing-api.soticcloud.net/stats/api/race/b0eae369-8d05-4b8e-9a2e-7de5871715b7?include=racePhase%2CraceBoats.raceBoatAthletes.person%2CraceBoats.invalidMarkResultCode%2CraceBoats.raceBoatIntermediates.distance&sortInclude%5BraceBoats.raceBoatIntermediates.ResultTime%5D=asc
@@ -193,8 +218,9 @@ class Race(Base): # https://world-rowing-api.soticcloud.net/stats/api/race/b0eae
     id = Column(Integer, primary_key=True)
     additional_id_ = Column(String)
     event_id = Column(Integer, ForeignKey("events.id"))
-    name = Column(String)
+    event    = relationship("Event", back_populates="races")
 
+    name = Column(String)
     date = Column(Date)
     phase_type = Column(String) # e.g. "Heat", "Final" // TODO: normalize?
     phase = Column(String) # e.g. "FA", "H3", "SA/B1", etc...
@@ -211,6 +237,9 @@ class Race(Base): # https://world-rowing-api.soticcloud.net/stats/api/race/b0eae
     rescheduled_from__ = Column(String) # low-prio TODO: Type unclear
     race_status__ = Column(String) # e.g. DisplayName "Official" id "182f6f15-8e78-41c3-95b3-8b006af2c6a1"
 
+    # relationships
+    race_boats = relationship("Race_Boat", back_populates="race")
+
 
 class Race_Boat(Base):
     __tablename__ = "raceboats"
@@ -218,7 +247,9 @@ class Race_Boat(Base):
     id = Column(Integer, primary_key=True)
     additional_id_ = Column(String)
     race_id = Column(Integer, ForeignKey("races.id"))
+    race    = relationship("Race", back_populates="race_boats")
     country_id = Column(Integer, ForeignKey("countries.id"))
+    country    = relationship("Country")
 
     # many-to-many relationship
     athletes = relationship("Athletes", secondary=boat_athlete_association_table)
@@ -238,6 +269,9 @@ class Race_Boat(Base):
     club_name__ = Column(String)
     # worldBestTimeId, OVRCode ?
 
+    # relationships
+    race_data = relationship("Race_Data", back_populates="raceboat")
+
 
 class Race_Data(Base):
     __tablename__ = "race_data"
@@ -245,6 +279,7 @@ class Race_Data(Base):
     # TODO: Unique: (boat_id && distance_meter) # https://stackoverflow.com/q/10059345
     # Multi Column Primary Key: https://stackoverflow.com/a/9036128
     raceboat_id = Column(Integer, ForeignKey("raceboats.id"), primary_key=True, autoincrement=False)
+    raceboat    = relationship("Race", back_populates="race_boats")
     distance_meter = Column(Integer, primary_key=True, autoincrement=False)
 
     # Data fields from JSON Web API aka "Intermediates"
@@ -261,14 +296,22 @@ class Race_Data(Base):
 #----------------------------------------------------------------------
 
 def create_tables():
+    engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/rowing", echo=True)
+
+    Session = sessionmaker(bind=engine)
+
     # create all tables (init) if they don't exist
     Base.metadata.create_all(engine, checkfirst=True)
 
     session = Session()
     session.commit()
 
+
 if __name__ == '__main__':
     print("-"*100, "Init DB")
+    create_tables()
+
+
     # from ..scraping_wr import api
     # from backend.scraping_wr import api
     # print(api)
@@ -279,5 +322,5 @@ if __name__ == '__main__':
     # import backend.scraping_wr.api
 
     # solution: run as module `python -m backend.model.model`
-    from .. scraping_wr import api
-    print("api", api)
+    # from .. scraping_wr import api
+    # print("api", api)
