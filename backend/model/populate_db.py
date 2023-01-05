901345b0d065eb6039fa9e437c4eb486d0f425cf
raphaelw <raphaelw@users.noreply.github.com>
2023-01-05 20:19:29 +0100

    begin db insertion code

--- backend/model/populate_db.py
+++ backend/model/populate_db.py
@@ -25,16 +25,29 @@ def drop_all_tables():
     model.Base.metadata.drop_all(engine)
 
 
+def insert_competition_worldrowing_com(competition):
+    pass
 
 
 if __name__ == '__main__':
     import argparse
+    from sys import exit as sysexit
+    import json
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--create", help="Create tables if not yet existing", action="store_true")
     parser.add_argument("-d", "--drop", help="Drop all tables described by the schema defined in model.py", action="store_true")
+    parser.add_argument("-i", "--insert", help="Import JSON data for a rowing competition")
     args = parser.parse_args()
     print(args)
+
+    if args.insert:
+        print("Load JSON file:", args.insert)
+        with open(args.insert, mode="r", encoding="utf-8") as fp:
+            competition = json.load(fp)
+
+        insert_competition_worldrowing_com(competition)
+        sysexit()
 
     if args.drop:
         print("----- Drop All Tables -----")
