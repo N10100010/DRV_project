00d427c5b1c065a3254058c71edc71bee71d812e
Faßbender Harri <hf020@hdm-stuttgart.de>
2023-01-06 10:39:08 +0100

    minor design fixes

--- frontend/src/views/MedallienspiegelView.vue
+++ frontend/src/views/MedallienspiegelView.vue
@@ -1,10 +1,12 @@
 <template>
+  <v-container class="pa-10" style="max-width: 1024px">
   <h1>Medallienspiegel</h1>
   <v-divider></v-divider>
   <v-container class="pa-0 mt-8" style="min-height: 500px">
  <v-alert color="info" icon="$info" class="mt-8">
     Hinweis: Seite befinet sich im Aufbau.
     </v-alert>
+  </v-container>
   </v-container>
 </template>
 
