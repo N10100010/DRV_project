7e7bbe97248affabead3fc033361afad747eda9a
raphaelw <raphaelw@users.noreply.github.com>
2023-01-23 22:13:50 +0100

    first half of prescrape logic

--- backend/procedures/scrape_service.py
+++ backend/procedures/scrape_service.py
@@ -1,15 +1,20 @@
 import logging
 import os
 from time import sleep
+import datetime
+
+from sqlalchemy import select
 
 from model import model
+from model import dbutils
 from scraping_wr import api
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-__SCRAPER_DEV_MODE = os.environ.get('DRV_SCRAPER_DEV_MODE','').strip() == '1'
-__SCRAPER_SLEEP_TIME_SECONDS = 60 * 60
+SCRAPER_DEV_MODE = os.environ.get('DRV_SCRAPER_DEV_MODE','').strip() == '1'
+SCRAPER_SLEEP_TIME_SECONDS = 60 * 60
+SCRAPER_YEAR_MIN = int(os.environ.get('SCRAPER_YEAR_MIN', '1990').strip())
 
 """ NOTES
 - [SCRAPE] Procedure
@@ -24,12 +29,51 @@ TODO
 
 """
 
+def _scrape_range_half_year_window():
+    today = datetime.date.today()
+    second_half = today.month > 6
+    if today.month > 6:
+        return today.year, today.year+1
+    return today.year-1, today.year
+
+def _scrape_range_full_year_window():
+    today = datetime.date.today()
+    return today.year-1, today.year+1
+
+def detect_wr_scrapes(session):
+    statement = select(model.Competition.id).where(model.Competition.additional_id_ != None) # TODO: Introduce a field for source 
+    rows = session.execute(statement).first()
+    detected = not rows == None
+    return detected
+
 def prescrape():
     logger = logging.getLogger("prescrape")
+    logger.info("Initialize Database")
+    dbutils.create_tables(model.engine)
 
-    logger.info("Fetch all competition heads and write to db")
-    result = api.get_competition_ids(single_fetch=False)
-    pass
+    year_min, year_max = _scrape_range_half_year_window()
+
+    with model.Scoped_Session() as session:
+        logger.info("Trying to detect an earlier World Rowing API prescrape")
+        wr_detected = detect_wr_scrapes(session)
+
+        if not wr_detected:
+            logger.info("No World Rowing API scrapes detected. Extending scrape range to max.")
+            year_min = SCRAPER_YEAR_MIN
+            if SCRAPER_DEV_MODE:
+                year_min = datetime.date.today().year-5
+
+        logger.info(f"Final decision for year range selection: {year_min}-{year_max}")
+        if year_min > year_max:
+            raise Exception(f"Year range is invalid: {year_min}-{year_max}")
+        
+        logger.info("Fetch all competition heads and write to db")
+        for year in range(year_min, year_max+1):
+            logger.info(f"BEGIN year={year}")
+            competitions_wr = api.get_competition_heads([year], single_fetch=False)
+            for competition_data in competitions_wr:
+                logger.info(f'''Adding year={year} competition="{competition_data.get('id')}" name="{competition_data.get('DisplayName')}"''')
+                sleep(1)
 
 def scrape():
     logger = logging.getLogger("scrape")
@@ -55,7 +99,7 @@ def maintain():
     logger.info("Overwrite in db")
 
 
-def scheduler(duration=__SCRAPER_SLEEP_TIME_SECONDS):
+def scheduler(duration=SCRAPER_SLEEP_TIME_SECONDS):
     logger = logging.getLogger("scheduler")
 
     logger.info(f"Waiting {duration} seconds ...")
