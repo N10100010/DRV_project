b18e724c482ebf67d05f69b90c8c844df34e542c
Faßbender Harri <hf020@hdm-stuttgart.de>
2023-02-02 16:58:19 +0100

    added frontend functionality for authentication

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-container :class="mobile ? 'px-5 main-container' : 'px-10 pt-0 main-container'">
+    <v-col cols="12" class="pa-0 pt-0" justify="center">
+      <h1 class="py-3">Login</h1>
+      <v-col cols="6" class="px-0">
+        <v-alert type="info" variant="tonal">
+          Die Seite ist passwortgeschützt. Bitte melde dich an um fortzufahren.
+        </v-alert>
+      </v-col>
+      <v-col cols="12" sm="8" md="4" class="px-0">
+        <v-form class="px-0">
+          <v-text-field
+              label="Username"
+              v-model="username"
+              variant="outlined"
+              density="compact"></v-text-field>
+          <v-text-field
+              label="Password"
+              type="password"
+              v-model="password"
+              variant="outlined"
+              density="compact"></v-text-field>
+          <v-btn color="#1369b0" style="color: white" @click="login">Login</v-btn>
+        </v-form>
+      </v-col>
+    </v-col>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: 'drv_test_user',
+      password: '000000'
+    }
+  },
+  created() {
+    this.attributes = this.calenderData
+    window.addEventListener('resize', this.checkScreen);
+    this.checkScreen();
+  },
+  methods: {
+    login() {
+
+      const token = 'test_token_value'
+      const expires = Date.now() + 7 * 24 * 60 * 60 * 1000
+
+      localStorage.setItem('token', token)
+      localStorage.setItem('expires', expires)
+
+      if (Date.now() >= localStorage.getItem('expires')) {
+        localStorage.removeItem('token')
+        localStorage.removeItem('expires')
+      } else {
+        console.log("Valid token found.")
+      }
+    },
+    checkScreen() {
+      this.windowWidth = window.innerWidth;
+      this.mobile = this.windowWidth <= 750
+      let navbarHeight = window.innerWidth < 750 ? '71.25px' : '160px';
+      document.documentElement.style.setProperty('--navbar-height', navbarHeight);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.main-container {
+  min-height: calc(100vh - (var(--navbar-height)) - 95px);
+}
+</style>
