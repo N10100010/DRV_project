89ae26b19e579ffe2301378dd579701f47b2af73
Faßbender Harri <hf020@hdm-stuttgart.de>
2023-02-06 21:53:37 +0100

    updated medaillenspiegel page according to new specification

--- frontend/src/stores/medaillenspiegelStore.js
+++ frontend/src/stores/medaillenspiegelStore.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import {defineStore} from "pinia";
 
+const COLORS = ['#0C67F7', '#93E9ED', '#E0A9FA', '#E0B696', '#E0FAAC', '#F0E95A'];
+
 export const useMedaillenspiegelState = defineStore({
     id: "medaillenspiegel",
     state: () => ({
@@ -100,13 +102,13 @@ export const useMedaillenspiegelState = defineStore({
                 }
             },
             "competition_category_ids": [
-                 { "displayName": "OG", "id": "89346342" },
-                { "displayName": "EM", "id": "89346362" },
-                { "displayName": "WCh", "id": "89346362" },
-                { "displayName": "WCI", "id": "89346362" },
-                { "displayName": "WCII", "id": "89346362" },
-                { "displayName": "WCIII", "id": "89346362" },
-                { "displayName": "LS", "id": "89346362" }
+                {"displayName": "OG", "id": "89346342"},
+                {"displayName": "EM", "id": "89346362"},
+                {"displayName": "WCh", "id": "89346362"},
+                {"displayName": "WCI", "id": "89346362"},
+                {"displayName": "WCII", "id": "89346362"},
+                {"displayName": "WCIII", "id": "89346362"},
+                {"displayName": "LS", "id": "89346362"}
             ],
             "runs": [{"displayName": "FA", "id": "89346342"}],
             "ranks": ["1", "2", "3", "4-6"],
@@ -330,30 +332,68 @@ export const useMedaillenspiegelState = defineStore({
         },
         data: [
             {
-                "filter_selection": {
-                    "start_date": "2020-06-16 14:12:00",
-                    "end_date": "2022-06-16 14:12:00",
-                    "results": 23,
-                    "nation_ioc": "GER (Deutschland)",
-                    "boat_classes": [
-                        {"M1x": "Men's Single Sculls"},
-                        {"M2x": "Men's Double Sculls"}
-                    ]
-                },
-                "table_data": {
-                    "rank": {
-                        "gold": 500,
-                        "silver": 400,
-                        "bronze": 300
+                "results": 4,
+                "start_date": "2020",
+                "end_date": "2022",
+                "events": "EM",
+                "category": "Olympisch",
+                "boat_classes": [
+                    {"M1x": "Men's Single Sculls"},
+                    {"M2x": "Men's Double Sculls"}
+                ],
+                "table_data": [
+                    {
+                        "nation_ioc": "GER",
+                        "rank": "1",
+                        "points": 900,
+                        "medals_gold": 2,
+                        "medals_silver": 5,
+                        "medals_bronze": 5,
+                        "medals_total": 17,
+                        "final_a": 12,
+                        "final_b": 6,
+                        "final_c": 0,
+                        "final_d": 0
                     },
-                    "points": 700,
-                    "medals_gold": 5,
-                    "medals_silver": 5,
-                    "medals_bronze": 5,
-                    "medals_total": 15,
-                    "final_a": 12,
-                    "final_b": 6
-                }
+                    {
+                        "nation_ioc": "FRA",
+                        "rank": "2",
+                        "points": 760,
+                        "medals_gold": 5,
+                        "medals_silver": 5,
+                        "medals_bronze": 5,
+                        "medals_total": 15,
+                        "final_a": 12,
+                        "final_b": 6,
+                        "final_c": 0,
+                        "final_d": 0
+                    },
+                    {
+                        "nation_ioc": "ITA",
+                        "rank": "3",
+                        "points": 700,
+                        "medals_gold": 2,
+                        "medals_silver": 3,
+                        "medals_bronze": 5,
+                        "medals_total": 10,
+                        "final_a": 12,
+                        "final_b": 6,
+                        "final_c": 0,
+                        "final_d": 0
+                    },
+                    {
+                        "nation_ioc": "RUS",
+                        "rank": "4",
+                        "points": 500,
+                        "medals_gold": 1,
+                        "medals_silver": 4,
+                        "medals_bronze": 6,
+                        "medals_total": 11,
+                        "final_a": 12,
+                        "final_b": 6,
+                        "final_c": 0,
+                        "final_d": 0
+                    }]
             }
         ]
     }),
@@ -365,30 +405,37 @@ export const useMedaillenspiegelState = defineStore({
             return state.filterOptions
         },
         getFilterSelection(state) {
-            return state.data[0].filter_selection
+            return state.data[0]
         },
         getTableData(state) {
-            let tableData = [["Platz", "Punkte", "Gold", "Silber", "Bronze", "Gesamt", "Finale A", "Finale B"]];
-            const valueKeys = ["rank", "points", "medals_gold", "medals_silver", "medals_bronze", "medals_total", "final_a", "final_b"];
-            let tempArray = []
-            for (const valueKey of valueKeys) {
-                tempArray.push(state.data[0].table_data[valueKey]);
+            let tableData = [["Platz", "Nation", "Punkte", "Gold", "Silber", "Bronze", "Medaillen", "Finale A", "Finale B", "Finale C", "Finale D"]];
+            const valueKeys = ["rank", "nation_ioc", "points", "medals_gold", "medals_silver", "medals_bronze", "medals_total", "final_a", "final_b", "final_c", "final_d"];
+            const data = state.data[0].table_data
+            for (const el of data) {
+                let temp1Array = []
+                for (const valueKey of valueKeys) {
+                    temp1Array.push(el[valueKey]);
+                }
+                tableData.push(temp1Array)
             }
-            tableData.push(tempArray)
             state.tableExport = tableData
             return tableData
         },
         getBarChartData(state) {
+            const data = state.data[0].table_data
+            let dataSets = []
+            const labels = ["Gold", "Silver", "Bronze", "Total"]
+            for (const [index, country] of data.entries()) {
+                dataSets.push({
+                    type: 'bar',
+                    label: country.nation_ioc,
+                    backgroundColor: COLORS[index % COLORS.length],
+                    data: [country.medals_gold, country.medals_silver, country.medals_bronze, country.medals_total]
+                })
+            }
             return {
-                labels: state.medal_bar_chart_data.labels,
-                datasets: [
-                    {
-                        type: 'bar',
-                        label: "Anzahl Medaillen",
-                        backgroundColor: '#5cc5ed',
-                        data: state.medal_bar_chart_data.data
-                    },
-                ]
+                labels: labels,
+                datasets: dataSets
             }
         },
     },
