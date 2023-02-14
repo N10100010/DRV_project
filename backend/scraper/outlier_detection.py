1e84b83cfdcf8ebaac3a52abf34393d65350a68d
Nick <nr034@hdm-stuttgart.de>
2023-02-14 17:52:41 +0100

    SCRAPING: outlier marking race data

--- backend/scraper/outlier_detection.py
+++ backend/scraper/outlier_detection.py
@@ -16,8 +16,107 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("outlier_detector")
 
 
-def outlier_detection(session:Session, boat_class: model.Boat_Class) -> None:
-    logger.info(f"Boat Class: {boat_class.abbreviation}")
+def outlier_detection_race_data(session:Session, boat_class: model.Boat_Class) -> None:
+    logger.info(f"Marking race_data for boat class: {boat_class.id}")
+    race_data_statement = (
+        select(
+            model.Race_Data.distance_meter,
+            model.Competition_Category.id.label("competition_category_id")
+        )
+        .join(model.Race_Data.race_boat)
+        .join(model.Race_Boat.race)
+        .join(model.Race.event)
+        .join(model.Event.competition)
+        .join(model.Competition.competition_category)
+        .where(model.Event.boat_class_id == boat_class.id)
+        .order_by(model.Race_Data.distance_meter)
+        .group_by(
+            model.Race_Data.distance_meter,
+            model.Competition_Category.id
+        )
+    )
+    race_data = session.execute(race_data_statement).all()
+
+    for row in race_data: 
+
+        percentiles_statement = (
+            select(
+                
+                    func.percentile_cont(OUTLIER_DETECTION_PERCENTILE_MIN).within_group(model.Race_Data.speed_meter_per_sec.desc()).label("percentile_smps_min"),
+                    func.percentile_cont(OUTLIER_DETECTION_PERCENTILE_MAX).within_group(model.Race_Data.speed_meter_per_sec.desc()).label("percentile_smps_max"),
+                    func.percentile_cont(OUTLIER_DETECTION_PERCENTILE_MIN).within_group(model.Race_Data.stroke.desc()).label("percentile_stroke_min"),
+                    func.percentile_cont(OUTLIER_DETECTION_PERCENTILE_MAX).within_group(model.Race_Data.stroke.desc()).label("percentile_stroke_max"),
+                
+            )
+            .join(model.Race_Data.race_boat)
+            .join(model.Race_Boat.race)
+            .join(model.Race.event)
+            .join(model.Event.competition)
+            .join(model.Competition.competition_category)
+            .where(
+                and_(
+                    model.Event.boat_class_id == boat_class.id,
+                    model.Race_Data.distance_meter == row.distance_meter,
+                    model.Competition_Category.id == row.competition_category_id,
+                    model.Race_Data.speed_meter_per_sec != 0
+                )
+            )
+        )
+
+        percentiles = session.execute(percentiles_statement).fetchall()[0]
+        if not all(percentiles): 
+            # if there is a None in percentiles, it means they could not have been calculated. 
+            # This is a result from no data present to calc the percentiles.
+            logger.info(
+                f"Could not calculate the percentiles. The folloing filters result in no data:",
+                f"\n - Event.boat_class_id: {boat_class.id}",
+                f"\n - Race_Data.distance_meter: {row.distance_meter}",
+                f"\n - Event.boat_class_id: {row.competition_category_id}",
+                "\n moving on to next distance_meter."
+            )
+            continue
+        else: 
+            min_smps, max_smps, min_stroke, max_stroke = sorted(percentiles)
+
+        to_adjust_statement = (
+            select(
+                model.Race_Data.race_boat_id,
+                model.Race_Data.distance_meter
+            )
+            .join(model.Race_Data.race_boat)
+            .join(model.Race_Boat.race)
+            .join(model.Race.event)
+            .where(
+                and_(
+                    model.Event.boat_class_id == boat_class.id,
+                    or_(
+                        ~model.Race_Data.speed_meter_per_sec.between(min_smps, max_smps),
+                        ~model.Race_Data.stroke.between(min_stroke, max_stroke),
+                    ),
+                    model.Race_Data.distance_meter == row.distance_meter,
+                    model.Competition_Category.id == row.competition_category_id
+                )
+            )
+        )
+
+        to_adjust = session.execute(to_adjust_statement).fetchall()
+        # todo: how to do a bulk-update? 
+        for race_boat_id, distance_meter in to_adjust: 
+            updt = update(model.Race_Data)
+            updt = updt.values({'is_outlier': True})
+            updt = updt.where(model.Race_Data.race_boat_id == race_boat_id)
+            updt = updt.where(model.Race_Data.distance_meter == distance_meter)
+
+            session.execute(updt)
+            session.commit()
+
+    return None
+
+
+
+def outlier_detection_result_data(session:Session, boat_class: model.Boat_Class) -> None:
+
+    logger.info(f"Marking result_data for boat class: {boat_class.id}")
 
     # https://docs.sqlalchemy.org/en/14/core/selectable.html#sqlalchemy.sql.expression.GenerativeSelect.group_by
     # func.xxx ---> https://www.postgresql.org/docs/8.2/functions-aggregate.html
