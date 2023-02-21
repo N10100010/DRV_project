7b6b801007e5e1c0903d8ff960340449a15b81d3
Julian <jf128@hdm-stuttgart.de>
2023-02-21 06:17:03 +0100

    added logos and mitwirkende link to footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,25 +1,62 @@
 <template>
-  <footer class="footer">
-    <v-container>
-      <RouterLink to="/impressum" style="color: white" class="pa-2">Impressum</RouterLink>
-      <RouterLink to="/datenschutz" style="color: white" class="pa-2">Datenschutz</RouterLink>
-      <RouterLink to="/hilfe" style="color: white" class="pa-2">Hilfe</RouterLink>
-    </v-container>
+  <footer class="footer" :style="mobile ? 'min-height: 100px' : 'min-height: 96px;'">
+    <v-container :class="mobile ? 'px-5 pt-2 pb-1 d-sm-flex' : 'px-10 pt-6 d-sm-flex flex-justify-between flex-items-center'">
+      <RouterLink to="/impressum" :class="mobile ? (minreached ? 'pl-2' : 'pt-3 pl-6 pr-3' ) : 'pt-3 pr-3'">Impressum</RouterLink>
+      <RouterLink to="/datenschutz" :class="minreached ? 'pl-2' : 'pa-3'">Datenschutz</RouterLink>
+      <RouterLink to="/hilfe" :class="minreached ? 'pl-2' : 'pa-3'">Hilfe</RouterLink>
+      <RouterLink to="/mitwirkende" :class="minreached ? 'pl-2' : 'pa-3'">Mitwirkende</RouterLink>
+      <v-spacer class="my-2"></v-spacer>
+        <a href="https://ai.hdm-stuttgart.de/" class="mr-6"><img src="@/assets/images/IAAI_Logo.svg" :width="minreached ? 55 : 69"></a>
+        <a href="https://www.hdm-stuttgart.de/" :class="mobile ? (minreached ? 'mr-0' : 'mr-5') :'mr-0'"><img src="@/assets/images/HDM_Logo.svg" :width="minreached ? 45 : 54"></a>
+      </v-container>
   </footer>
 </template>
 
+<script>
+export default {
+  created(){
+    window.addEventListener('resize', this.checkScreen);
+    this.checkScreen();
+    window.scrollTo(0, 0)
+  },
+  methods: {
+    checkScreen() {
+      this.windowWidth = window.innerWidth;
+      this.mobile = this.windowWidth < 890;
+      this.minreached = this.windowWidth < 600;
+      let navbarHeight = window.innerWidth < 890 ? '71.25px' : '160px';
+      document.documentElement.style.setProperty('--navbar-height', navbarHeight);
+    }
+  },
+  data() {
+    return {
+      mobile: false,
+      minreached: false
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .footer {
   text-align: center;
   font-style: italic;
-  margin: auto;
+  align-items: center;
+  display: flex;
+  margin: 0 auto;
   left: 0;
   right: 0;
   width: 100%;
   background-color: #1369b0;
   justify-self: center;
-  min-height: 100px;
   bottom: 0;
+}
+
+a {
+  color: rgba(255,255,255,0.75);
+}
+
+a:hover {
+  color: white;
 }
 </style>
---
+++ frontend/src/views/MitwirkendeView.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container :class="mobile ? 'px-5 py-2 main-container' : 'px-10 pt-0 main-container'">
+    <v-col cols="12" class="d-flex flex-row px-0" style="align-items: center">
+      <h1>Mitwirkende</h1>
+    </v-col>
+    <v-divider></v-divider>
+    <v-container class="pa-0 mt-2" style="min-height: 450px">
+      <v-col class="pl-0 pr-0">
+      <h2><i>Herausgeber:</i></h2>
+      <p>Deutscher Ruderverband<br />
+        Markus Last<br />
+        Ferdinand-Wilhelm-Fricke-Weg 10<br />
+        30169 Hannover<br />
+        Telefon: +49 (0) 4541 8643 46<br />
+        E-Mail: <a href="mailto:markus.last@rudern.de">markus.last@rudern.de</a></p>
+      <br>
+      <h2>Rechtliche Hinweise</h2>
+      <p>Wichtiger Hinweis zu allen Links. Wir sind nicht für die Inhalte fremder Seiten verantwortlich, die über einen
+      Link erreicht werden. Diese Erklärung gilt für alle auf <a href="https://drv-stats.up.railway.app/">https://drv-stats.up.railway.app/</a> angebrachten Links.</p>
+      <br>
+      <h2>Haftungsausschluss</h2>
+      <p>Die auf diesen Websites bereitgestellten Informationen wurden geprüft und werden regelmäßig aktualisiert. Es kann
+      jedoch keine Garantie dafür übernommen werden, dass alle Angaben zu jeder Zeit vollständig, richtig und in letzter
+      Aktualität dargestellt sind. Dies gilt insbesondere für alle Links zu anderen Websites, auf die direkt oder
+      indirekt verwiesen wird. Alle Angaben können ohne vorherige Ankündigung ergänzt, entfernt oder geändert werden.</p>
+      <br>
+      <h2>Urheberrecht</h2>
+      <p>Die auf <a href="https://drv-stats.up.railway.app/">https://drv-stats.up.railway.app/</a> veröffentlichen Informationen stammen aus öffentlich zugänglichen
+      Wettkampf- und Ergebnisinformationen. Für die Verwendung von dem aufgearbeiteten Text- oder Bildmaterial bedarf es
+      dennoch der vorherigen Zustimmung durch den Seitenbetreiber.</p>
+      </v-col>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+export default {
+  created(){
+    window.addEventListener('resize', this.checkScreen);
+    this.checkScreen();
+    window.scrollTo(0, 0)
+  },
+  methods: {
+    checkScreen() {
+      this.windowWidth = window.innerWidth;
+      this.mobile = this.windowWidth < 890
+      let navbarHeight = window.innerWidth < 890 ? '71.25px' : '160px';
+      document.documentElement.style.setProperty('--navbar-height', navbarHeight);
+    }
+  },
+  data() {
+    return {
+      mobile: false,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main-container {
+  min-height: calc(100vh - (var(--navbar-height)) - 95px);
+}
+</style>
