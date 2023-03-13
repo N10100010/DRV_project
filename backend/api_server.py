39637cdb2afa77ed229c3e672132eea1e517d5a0
raphaelw <raphaelw@users.noreply.github.com>
2023-03-13 18:01:08 +0100

    Update api_server.py

--- backend/api_server.py
+++ backend/api_server.py
@@ -1,9 +1,9 @@
 # This file is the entry point for the Backend API Server based on flask.
 # This file just provides the flask-based WSGI object called "app" by convention.
 
-import os
 from api.app import app
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
