eaa3f64743ee1754f5cd7ba55ab1faa0917fabc1
raphaelw <raphaelw@users.noreply.github.com>
2023-03-13 17:50:29 +0100

    remove deprecated skeleton code

--- backend/scraping_wr/api.py
+++ backend/scraping_wr/api.py
@@ -204,84 +204,17 @@ def select_pdf_(pdfUrls: list, title: str) -> Union[dict, None]:
     return None # or raise error?
 
 
-
-# Following functions are kinde of deprecated:
-# PDF-Scrape, Parsing and Merge will be implemented in a second pass (aka background/cron process)
-"""
-def merge_intermediates(race, race_data):
-    mappings = []
-    '''mappings is a list of tuples of the form (race_idx, extra_data_idx)
-    mappings = [ (1,0), (0,1) ]
-    '''
-
-    # TODO: Consider the case where raceBoats is empty but there is PDF data available
-    for race_boat_idx, race_boat in enumerate(race.get('raceBoats', [])):
-        for rd_idx, rd_item in race_data:
-            if rd_item.get('boat_name','').strip().upper() == race_boat.get('DisplayName','').strip().upper():
-                mappings.append( (race_boat_idx, rd_idx) )
-
-    # apply the mapping
-    for mapping in mappings:
-        race_idx, race_data_idx = mapping
-        race[race_idx]['pdf_parsed_intermediates'] = race_data[race_data_idx]
-
-
-def merge_race_data(race, race_data):
-    mappings = []
-    '''mappings is a list of tuples of the form (race_idx, extra_data_idx)
-    mappings = [ (1,0), (0,1) ]
-    '''
-
-    # TODO: Consider the case where raceBoats is empty but there is PDF data available
-    for race_boat_idx, race_boat in enumerate(race.get('raceBoats', [])):
-        for rd_idx, rd_item in race_data:
-            if rd_item.get('boat_name','').strip().upper() == race_boat.get('DisplayName','').strip().upper():
-                mappings.append( (race_boat_idx, rd_idx) )
-
-    # apply the mapping
-    for mapping in mappings:
-        race_idx, race_data_idx = mapping
-        race[race_idx]['pdf_parsed_race_data'] = race_data[race_data_idx]
-"""
-
 def get_by_competition_id_(comp_ids: Union[str, list[str]], verbose: bool = False, parse_pdf=False) -> dict:
     """
     Stripped down version of get_by_competition_id()
     """
-
     comp_id = comp_ids if isinstance(comp_ids, str) else comp_ids[0]
 
     comp_data = ut_wr.load_json(WR_BASE_URL + WR_ENDPOINT_COMPETITION + comp_id + WR_INCLUDE_EVERYTHING)
 
-    for event_idx, event in tqdm( enumerate(comp_data.get('events', [])) ):
-        for race_idx, race in enumerate(event.get('races', [])):
-            #logger.info(f"event_idx {event_idx} race_idx {race_idx}")
-
-            pdf_info_race_data = select_pdf_(race.get('pdfUrls', []), 'race data')
-            pdf_info_results = select_pdf_(race.get('pdfUrls', []), 'results')
-
-            if parse_pdf and pdf_info_results:
-                pdf_url = pdf_info_results.get('url')
-                logger.info(f"pdf_info_results {pdf_url}")
-                # trigger parsing
-                results_data = pdf_result_data.extract_table_data_from_pdf([pdf_url])[0]
-
-                # TODO: merge using merge_intermediates()
-
-                # write all at race level TODO: rem
-                race['raceIntermediates_'] = results_data
-
-            if parse_pdf and pdf_info_race_data:
-                pdf_url = pdf_info_race_data.get('url')
-                logger.info(f"pdf_info_race_data {pdf_url}")
-                race_data = pdf_race_data.extract_table_data_from_pdf([pdf_url])[0]
-
-                # TODO: merge using merge_race_data()
-                # merge_race_data(race, race_data[0])
-
-                race['raceData_'] = race_data
-
-            # print("race_data_pdf_info", race_data_pdf_info['url'])
+    if parse_pdf:
+        # PDF parsing/matching was implemented elsewhere. See Scraper.
+        raise NotImplementedError
 
     return comp_data
 
